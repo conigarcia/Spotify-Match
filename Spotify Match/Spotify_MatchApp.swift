aa29e750e3832bc18034f19f07c8647fe91c9012
Constanza Garcia <conigarciadiaz@gmail.com>
2024-08-20 19:20:35 -0300

    WIP: App background

--- Spotify Match/Spotify_MatchApp.swift
+++ Spotify Match/Spotify_MatchApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Spotify_MatchApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
---
+++ Spotify Match/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Spotify Match
+//
+//  Created by coni garcia on 20/08/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        ZStack {
+            AppBackgroundView()
+            CardDeckView()
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
