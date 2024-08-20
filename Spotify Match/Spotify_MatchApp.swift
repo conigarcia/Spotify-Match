0cba1e25dc00c9fb2cd18f9f294eb7a65e54b446
Constanza Garcia <conigarciadiaz@gmail.com>
2024-08-20 12:42:31 -0300

    Initial Commit

---
+++ Spotify Match/Spotify_MatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_MatchApp.swift
+//  Spotify Match
+//
+//  Created by coni garcia on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_MatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
